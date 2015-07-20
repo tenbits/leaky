012fdaf65a2d6dbf8ad1dfad708815f3805c43b6
Alex Kit <source.kit@gmail.com>
2015-07-21 00:25:25 +0200

    add (input typing) heaps before and after

--- test/tests.js
+++ test/tests.js
@@ -20,78 +20,54 @@ describe('memory tests', function() {
     return this.driver.quit();
   });
 
-  it('inputs should not leak when added and removed', function() {
-    this.driver.get('file://' + path.join(__dirname, 'examples/', 'inputs.html'));
-
-    leaky.getCounts(this.driver)
-    .then(this.results.push.bind(this.results));
-
-    for (var i = 0; i < 20; ++i) {
-      this.driver.findElement(webdriver.By.css('#add-remove')).click();
-    }
-
-    return leaky.getCounts(this.driver)
-    .then(function(data) {
-      assert.equal(this.results[0].nodes, data.nodes,
-          'node count should match');
-    }.bind(this));
-  });
-
-  it('should not leak on initial typing', function() {
-    this.driver.get('file://' + path.join(__dirname, 'examples/', 'input_typeAndClear.html'));
-
-    leaky.getCounts(this.driver)
-    .then(this.results.push.bind(this.results));
-
-    this.driver.findElement(webdriver.By.css('input')).sendKeys('A');
-
-    return leaky.getCounts(this.driver)
-    .then(function(data) {
-      assert.equal(this.results[0].nodes, data.nodes,
-          'node count should match');
-    }.bind(this))
-  });
-
   it('should not leak on typing and clearing', function() {
+    this.timeout(120 * 1000)
     this.driver.get('file://' + path.join(__dirname, 'examples/', 'input_typeAndClear.html'));
 
-    leaky.getCounts(this.driver)
-    .then(this.results.push.bind(this.results));
+    leaky
+      .getCounts(this.driver)
+      .then(this.results.push.bind(this.results));
 
-    for (var i = 0; i < 5; ++i) {
-      get.call(this,  'input').sendKeys('Baz');
+    this
+      .driver
+      .executeScript(':takeHeapSnapshot')
+      .then(function(json){
+        writeHeap('before', json);
+      })
+    for (var i = 0; i < 200; ++i) {
+      get.call(this,  'input').sendKeys('B' + i);
       get.call(this, '#clear').click();
     }
+    this.driver
+      .executeScript(':takeHeapSnapshot')
+      .then(function(json){
+        writeHeap('after', json);
+      })
+
     function get(selector) {
-      return this.driver.findElement(webdriver.By.css(selector)); 
+      return this.driver.findElement(webdriver.By.css(selector));
     }
 
-    return leaky.getCounts(this.driver)
-    .then(function(data) {
-      assert.equal(this.results[0].nodes, data.nodes,
-          'node count should match');
-    }.bind(this));
+    return leaky
+      .getCounts(this.driver)
+      .then(function(data) {
+        assert.equal(this.results[0].nodes, data.nodes,
+            'node count should match');
+      }.bind(this));
   });
 
-  it('shows leaks', function() {
-    this.driver.get('file://' + path.join(__dirname, 'examples/', 'leaking.html'));
-    leaky.getCounts(this.driver)
-    .then(this.results.push.bind(this.results));
-
-    for (var i = 0; i < 4; ++i) {
-      this.driver.findElement(webdriver.By.css('#leak')).click();
-    }
-
-    leaky.getCounts(this.driver)
-    .then(function(data) {
-      assert.notEqual(this.results[0].nodes, data.nodes, 'node count does not match');
-    }.bind(this));
-
-    this.driver.findElement(webdriver.By.css('#clean')).click();
-
-    return leaky.getCounts(this.driver)
-    .then(function(data) {
-      assert.equal(this.results[0].nodes, data.nodes, 'node count does not grow');
-    }.bind(this));
-  });
 });
+
+function writeHeap(filename, json) {
+    var order = ['snapshot', 'nodes', 'edges', 'trace_function_infos', 'trace_tree', 'samples', 'strings'];
+    var open = '{\n', close = '\n}\n';
+    var keys = [];
+    for (var key in json) {
+      var fn = key === 'snapshot' ? 'unshift' : 'push';
+      var index = order.indexOf(key);
+      keys[index] = '"' + key + '":' + JSON.stringify(json[key]);
+    }
+    var str = open + keys.join('\n,\n') + close;
+
+    require('fs').writeFileSync(filename + '.heapsnapshot', str);
+}
